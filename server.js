9c91fa7a818b17a8d699e75bf264a750758a8982
nikolarendak <nikola.rendakov@sourcico.com>
2025-01-24 07:20:50 -0500

    Initial commit with site files

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
